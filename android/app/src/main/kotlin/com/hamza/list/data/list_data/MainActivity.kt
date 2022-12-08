1d43d95dac0866aba4c8f075fc7aee319edf3469
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-12-08 20:35:03 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/hamza/list/data/list_data/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hamza.list.data.list_data
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: QuoteList(),
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+
+  List<String> quotes = [
+    "Be yourself; everyone else is already taken.",
+    "Nothing else",
+    "Do the hard work even when you don't feel like it"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: const Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => (Text(quote))).toList(),
+      ),
+    );
+  }
+}
